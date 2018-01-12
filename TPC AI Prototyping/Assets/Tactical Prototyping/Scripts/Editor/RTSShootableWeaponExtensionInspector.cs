abbc97720527711cc260308e1ba6b6aba9bf39e9
Tanner555 <theman989898@gmail.com>
2018-01-11 23:01:04 -0800

    [Error Prone] Adds inherited classes from shootable weapon
    Causes errors from changing default prefabs settings when inheriting
    from the shootable weapon classes. Consider Reverting Changes to fix
    these errors.

---
+++ TPC AI Prototyping/Assets/Tactical Prototyping/Scripts/Editor/RTSShootableWeaponExtensionInspector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Opsive.ThirdPersonController.Editor;
+
+namespace RTSPrototype
+{
+    [CustomEditor(typeof(RTSShootableWeapon))]
+    public class RTSShootableWeaponExtensionInspector : ShootableWeaponInspector
+    {
+
+    }
+}
