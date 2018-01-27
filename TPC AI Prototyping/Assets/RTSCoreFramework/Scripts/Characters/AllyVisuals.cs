237f2f5dae48adfe643aa1c6d263097a22cd9c71
Tanner555 <theman989898@gmail.com>
2018-01-27 02:15:23 -0800

    Created Waypoint Notifier when Current Player Command Moves

--- TPC AI Prototyping/Assets/RTSCoreFramework/Scripts/Characters/AllyVisuals.cs
+++ TPC AI Prototyping/Assets/RTSCoreFramework/Scripts/Characters/AllyVisuals.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RTSCoreFramework
 {
     public class AllyVisuals : MonoBehaviour
     {
-
+        #region PropsAndFields
         [Header("Ally Highlighting")]
         public Color selAllyColor;
         public Color selEnemyColor;
         public Light SelectionLight;
+        [Header("Ally Waypoint Navigation")]
+        public Material waypointRendererMaterial;
+        private LineRenderer waypointRenderer;
 
         RTSGameMaster gamemaster
         {
@@ -34,13 +38,30 @@ namespace RTSCoreFramework
             get { return thisAlly.bIsInGeneralCommanderParty; }
         }
 
-        bool cameraIsMoving = false;
+        //NavMesh used for Waypoint Rendering
+        NavMeshAgent myNavMesh
+        {
+            get
+            {
+                if (_myNavMesh == null)
+                    _myNavMesh = GetComponent<NavMeshAgent>();
 
+                return _myNavMesh;
+            }
+        }
+        NavMeshAgent _myNavMesh = null;
+
+        bool cameraIsMoving = false;
+        #endregion
+
+        #region UnityMessages
         private void OnDisable()
         {
             npcMaster.OnHoverOver -= OnCursEnter;
             npcMaster.OnHoverLeave -= OnCursExit;
             npcMaster.EventNpcDie -= HandleDeath;
+            npcMaster.EventCommandMove -= SetupWaypointRenderer;
+            npcMaster.EventFinishedMoving -= DisableWaypointRenderer;
             gamemaster.GameOverEvent -= HandleGameOver;
             gamemaster.EventEnableCameraMovement -= HandleCameraMovement;
             uiMaster.EventAnyUIToggle -= HandleUIEnable;
@@ -52,10 +73,13 @@ namespace RTSCoreFramework
             npcMaster.OnHoverOver += OnCursEnter;
             npcMaster.OnHoverLeave += OnCursExit;
             npcMaster.EventNpcDie += HandleDeath;
+            npcMaster.EventCommandMove += SetupWaypointRenderer;
+            npcMaster.EventFinishedMoving += DisableWaypointRenderer;
             gamemaster.GameOverEvent += HandleGameOver;
             gamemaster.EventEnableCameraMovement += HandleCameraMovement;
             uiMaster.EventAnyUIToggle += HandleUIEnable;
         }
+        #endregion
 
         #region CursorHoverandExit
         void OnCursEnter(rtsHitType hitType, RaycastHit hit)
@@ -80,6 +104,53 @@ namespace RTSCoreFramework
         }
 
         #endregion
+
+        #region Handlers
+        void SetupWaypointRenderer(Vector3 _point)
+        {
+            Invoke("WaitToSetupWaypointRenderer", 0.1f);
+            
+        }
+
+        void WaitToSetupWaypointRenderer()
+        {
+            if (myNavMesh == null || myNavMesh.path == null ||
+                npcMaster.bIsAIMoving)
+                return;
+
+            if (waypointRenderer != null && waypointRenderer.enabled == false)
+            {
+                waypointRenderer.enabled = true;
+            }
+            else if (waypointRenderer == null)
+            {
+                waypointRenderer = this.gameObject.AddComponent<LineRenderer>();
+                if (waypointRendererMaterial != null)
+                    waypointRenderer.material = waypointRendererMaterial;
+
+                waypointRenderer.startWidth = 0.05f;
+                waypointRenderer.endWidth = 0.05f;
+                waypointRenderer.startColor = Color.yellow;
+                waypointRenderer.endColor = Color.yellow;
+            }
+
+            var path = myNavMesh.path;
+
+            waypointRenderer.positionCount = path.corners.Length;
+
+            for (int i = 0; i < path.corners.Length; i++)
+            {
+                waypointRenderer.SetPosition(i, path.corners[i]);
+            }
+        }
+
+        void DisableWaypointRenderer()
+        {
+            if(waypointRenderer != null)
+            {
+                waypointRenderer.enabled = false;
+            }
+        }
 
         void HandleDeath()
         {
@@ -112,5 +183,7 @@ namespace RTSCoreFramework
                 SelectionLight.enabled = false;
             }
         }
+        #endregion
+
     }
 }
