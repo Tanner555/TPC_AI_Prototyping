e733e38e8f05e2eb82f03a08a9549177d667fc08
Tanner555 <theman989898@gmail.com>
2018-04-17 09:42:32 -0700

    Created Main Menu with working buttons that load or quit the game

---
+++ TPC AI Prototyping/Assets/RTSCoreFramework/Scripts/ExtraFeatures/RTSMainMenuHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RTSCoreFramework
+{
+    /// <summary>
+    /// Temporary Class For Handling the Main Menu
+    /// Will Likely Replace in the Future with a Global
+    /// Game Manager Class
+    /// </summary>
+    public class RTSMainMenuHandler : MonoBehaviour
+    {
+        [SerializeField]
+        public Object LoadScene;
+
+        public void Btn_PlayGame()
+        {
+            if (LoadScene != null)
+            {
+                SceneManager.LoadScene(LoadScene.name);
+            }
+            else
+            {
+                Debug.Log("Please drag a scene into the main menu handler");
+            }
+        }
+
+        public void Btn_QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
