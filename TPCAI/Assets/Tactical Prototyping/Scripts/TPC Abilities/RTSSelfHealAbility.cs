84e8bb8d47c95fe9e8d4d9b0d5d683233b7933b0
Tanner555 <theman989898@gmail.com>
2018-07-28 07:22:34 -0700

    Created a few TPC Abilities for animations

---
+++ TPCAI/Assets/Tactical Prototyping/Scripts/TPC Abilities/RTSSelfHealAbility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTSCoreFramework;
+using Opsive.ThirdPersonController.Abilities;
+
+public class RTSSelfHealAbility : Ability {
+
+    RTSGameMode gamemode
+    {
+        get { return RTSGameMode.thisInstance; }
+    }
+
+    public override string GetDestinationState(int layer)
+    {
+        if (layer != m_AnimatorMonitor.BaseLayerIndex && layer != m_AnimatorMonitor.UpperLayerIndex &&
+            !m_AnimatorMonitor.ItemUsesAbilityLayer(this, layer))
+        {
+            return string.Empty;
+        }
+
+        return "SelfHeal.Movement";
+    }
+}
