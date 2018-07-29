f0659eae8b3239e084c503587806b747e8e68207
Tanner555 <theman989898@gmail.com>
2018-07-29 00:41:56 -0700

    Added A few Ability TPC classes that'll eventually replace the rtscore ability classes

---
+++ TPCAI/Assets/Tactical Prototyping/Scripts/Special Abilities/AbilityBehaviourTPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.ThirdPersonController.Abilities;
+using RTSCoreFramework;
+
+namespace RTSPrototype
+{
+    public class AbilityBehaviourTPC : MonoBehaviour
+    {
+        
+    }
+}
