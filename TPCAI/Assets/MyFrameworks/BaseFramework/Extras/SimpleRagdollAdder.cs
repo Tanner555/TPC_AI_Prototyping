c474863aeb89451f35d61238c0d64155c383dddc
Tanner555 <theman989898@gmail.com>
2018-08-13 07:54:56 -0700

    Created A Ragdoll Adder Script To Create A Ragdoll For My Models

---
+++ TPCAI/Assets/MyFrameworks/BaseFramework/Extras/SimpleRagdollAdder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseFramework
+{
+    /// <summary>
+    /// Simply Attach To A Temporary Gameobject, Assign
+    /// The Character, And Press The Button To Create a Ragdoll
+    /// </summary>
+    public class SimpleRagdollAdder : MonoBehaviour
+    {
+        [Header("Character Ragdoll Will Be Added To")]
+        public GameObject myCharacter = null;
+    }
+}
